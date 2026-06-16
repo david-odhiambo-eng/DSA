c341432d25f34d0021ffcf23aaa31b1d7981f64d
david-odhiambo-eng <odhiambodavid305@gmail.com>
2026-06-16 06:22:53 +0300

    adding btwn in a linked list

--- day_two/linked_list.py
+++ day_two/linked_list.py
@@ -6,15 +6,15 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    
+
     def traverse(self):
-        n = self.head
-        if n == None:
+        if self.head == None:
             print('List is empty')
         else:
+            n = self.head
             while n != None:
                 print(f'{n.data}-->',end='')
-                n = self.head.link
+                n = n.link
 
     def add_start(self, data):
         node = Node(data)
@@ -26,64 +26,34 @@ class LinkedList:
             self.head = node
 
     def add_end(self, data):
-        n = self.head
-        if n == None:
+        if self.head == None:
             self.add_start(data)
         else:
-            while n != None:
-                self.head = self.head.link
+            n = self.head
+            while n.link != None:
+                n = n.link
             node = Node(data)
-            self.head = node
+            n.link = node
             node.link = None
+
+    def add_between(self, data, value):
+        n = self.head
+        while n.data != value:
+            n = n.link
+            if n.data == value:
+                break
+        if n.data == None:
+            print('Not found')
+        else:
+            node = Node(data)
+            node.link = n
+            n = node
+
+
+node1 = LinkedList()
+node1.add_start(10)
+node1.add_end(100)
+node1.add_start(30)
+node1.add_between(219,2)
+node1.traverse()
             
-
-            
-        node = Node(data)
-
-link = LinkedList()
-link.add_start(34)
-link.add_start(55)
-link.add_start(101)
-link.add_end(250)
-link.add_end(20)
-link.add_end(45)
-link.traverse()
-
-            
-
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.link = None
-
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-
-#     def traverse(self):
-#         if self.head == None:
-#             print('List is empty')
-#         else:
-#             n = self.head
-#             #we use while if we know the stopping condition
-#             while n != None:
-#                 print(n.data,end='-->')
-#                 n = n.link
-
-#     def add_start(self, data):
-#         #We first create a node
-#         #By default, a created node has data and link None
-#         node1 = Node(data)
-#         #since the link is null, we point it to the head, which stores a reference to the initial first node
-#         node1.link = self.head
-#         #we then change the head to point to the added node
-#         self.head = node1# now head reads the location that was assigned during creating node
-
-# link = LinkedList()
-# link.add_start(34)
-# link.traverse()
-
-
-
-
-
