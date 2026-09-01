86c41e34d03e05050a8de172cfae42e57c89aa68
david-odhiambo-eng <odhiambodavid305@gmail.com>
2026-09-01 17:30:46 +0300

    youtube short

---
+++ linked_list_series/sum/taken/taken.py
@@ -0,0 +1,13 @@
+suspects = ['Jane', 'Mike', 'Dalton']
+target = 'Kevin'
+for suspect in suspects:
+    if suspect == target:
+        print('Found you...')
+    else:
+        print('''
+            I will look for you,
+            I will find you and
+            I will kill you
+             
+        ''')
+      
